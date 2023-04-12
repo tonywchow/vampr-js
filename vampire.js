34ca37c66a42ac92bbcaf96b415c7c80acfa83fe
Tony Chow <tchow9@gmail.com>
2023-04-11 20:55:15 -0700

    Initial commit

--- vampire.js
+++ vampire.js
@@ -2,48 +2,57 @@ class Vampire {
   constructor(name, yearConverted) {
     this.name = name;
     this.yearConverted = yearConverted;
-    this.offspring = [];
     this.creator = null;
+    this.offspring = [];
   }
 
   /** Simple tree methods **/
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampire = 0;
+    let currentVampire = this;
 
+    while (currentVampire.boss) {
+      currentVampire = currentVampire.boss;
+      numberOfVampire++;
+    }
+    return numberOfVampire;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (vampire.creator === null) {
+      return false;
+    }
+    if (this.offspring.length !== 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /** Tree traversal methods **/
 
   // Returns the vampire object with that name, or null if no vampire exists with that name
-  vampireWithName(name) {
-    
-  }
+  vampireWithName(name) {}
 
   // Returns the total number of vampires that exist
-  get totalDescendents() {
-    
-  }
+  get totalDescendents() {}
 
   // Returns an array of all the vampires that were converted after 1980
-  get allMillennialVampires() {
-    
-  }
+  get allMillennialVampires() {}
 
   /** Stretch **/
 
@@ -52,10 +61,7 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
-
-  }
+  closestCommonAncestor(vampire) {}
 }
 
 module.exports = Vampire;
-
